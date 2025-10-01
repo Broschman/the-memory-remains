12f514f6b5d86c77a51e74ac8906a79c9b362915
Broschman <ondra.brosch.ob@gmail.com>
2025-10-01 12:55:01 +0200

    Update kruznice.py¨

--- kruznice.py
+++ kruznice.py
@@ -93,7 +93,7 @@ if st.button("Uložit graf a parametry do PDF"):
 
     # rovnou nabídne ke stažení
     with open(pdf_file.name, "rb") as f:
-        st.download_button(
+        st.download_button()
             label="Stáhnout PDF",
             data=f,
             file_name="kruznice.pdf",
